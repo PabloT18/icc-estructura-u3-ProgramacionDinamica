3b14c5f1aab7701172867b3a6c54278ba5ca5596
PabloTorres <ptorresp@ups.edu.ec>
2024-07-10 15:51:22 -0500

    ADD: methods

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -24,22 +26,56 @@ public class App {
 
     }
 
-    // Normal recursive method
+    // FORMA NORMAL RECURSIVA
     public static int fibonacci(int n) {
-        // TODO: Implement this method
-        return 0;
+
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
-    // First caching method: Using a Map
+    // PRIMERA FORMA DE HACERLO CON MEMORIA
+
+    private static Map<Integer, Integer> cache = new HashMap<>();
+
     public static int fibonacciWithCaching(int n) {
-        // TODO: Implement this method
-        return 0;
+        if (n <= 1) {
+            return n;
+        }
+
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        }
+
+        int result = fibonacciWithCaching(n - 1) + fibonacciWithCaching(n - 2);
+        cache.put(n, result);
+        return result;
     }
 
-    // Second caching method: Using an array
+    // SEGUNADA FORMA DE HACERLO CON MEMORIA
+
+    // Método para calcular el n-ésimo número de la secuencia de Fibonacci
     public static int fibonacciCaching(int n) {
-        // TODO: Implement this method
-        return 0;
+        int[] cache = new int[n + 1]; // Utilizamos un arreglo para almacenar los resultados previamente calculados
+        return fibonacciMemoria(n, cache);
+    }
+
+    // Método auxiliar para calcular el n-ésimo número de Fibonacci utilizando
+    // memorizaci
+    private static int fibonacciMemoria(int n, int[] cache) {
+        if (n <= 1) {
+            return n;
+        }
+
+        // Si el valor ya fue calculado previamente, lo retornamos desde el caché
+        if (cache[n] != 0) {
+            return cache[n];
+        }
+        // Si el valor no ha sido calculado, lo calculamos recursivamente y lo
+        // almacenamos en el caché
+        cache[n] = fibonacciMemoria(n - 1, cache) + fibonacciMemoria(n - 2, cache);
+        return cache[n];
     }
 
     public static void runEjercicio() {
